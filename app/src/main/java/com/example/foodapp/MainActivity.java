d5486e65aaaca91e3a3a15dbb14429190ac763d6
NTHung117 <nguyentienhung117@gmail.com>
2023-03-09 23:52:17 +0700

    Home View

--- app/src/main/java/com/example/foodapp/MainActivity.java
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import androidx.appcompat.widget.Toolbar;
+import android.widget.ViewFlipper;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    ViewFlipper viewFlipper;
+    RecyclerView recyclerView;
+    NavigationView navigationView;
+    ListView listViewManHinhChinh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AnhXa();
+    }
+
+    private void AnhXa() {
+        toolbar = findViewById(R.id.toolbarHome);
+        viewFlipper = findViewById(R.id.viewFlipper);
+        recyclerView = findViewById(R.id.recyclerView);
+        navigationView = findViewById(R.id.navigationView);
+        listViewManHinhChinh = findViewById(R.id.listViewManHinhChinh);
     }
 }
