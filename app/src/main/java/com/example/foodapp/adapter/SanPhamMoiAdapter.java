00500a92fa604e44eb436940ed6dfc1f98f8f1b2
NTHung117 <nguyentienhung117@gmail.com>
2023-03-14 00:18:41 +0700

    Tạo Adapter, model cho recycleview của sản phẩm mới ở màn hình chính

---
+++ app/src/main/java/com/example/foodapp/adapter/SanPhamMoiAdapter.java
@@ -0,0 +1,61 @@
+package com.example.foodapp.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.foodapp.R;
+import com.example.foodapp.model.SanPhamMoi;
+
+import java.util.List;
+
+public class SanPhamMoiAdapter extends RecyclerView.Adapter<SanPhamMoiAdapter.MyViewHolder> {
+    Context context;
+    List<SanPhamMoi> array;
+
+    public SanPhamMoiAdapter(Context context, List<SanPhamMoi> array) {
+        this.context = context;
+        this.array = array;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_sp_moi,parent, false);
+
+        return new MyViewHolder(item);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        SanPhamMoi sanPhamMoi = array.get(position);
+        holder.textTen.setText(sanPhamMoi.getTensanpham());
+        holder.textGia.setText(sanPhamMoi.getGiasanpham());
+        Glide.with(context).load(sanPhamMoi.getHinhanh()).into(holder.imageHinhAnh);
+    }
+
+    @Override
+    public int getItemCount() {
+        return array.size();
+    }
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView textTen, textGia;
+        ImageView imageHinhAnh;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textTen = itemView.findViewById(R.id.itemspmoi_ten);
+            textGia = itemView.findViewById(R.id.itemspmoi_gia);
+            imageHinhAnh = itemView.findViewById(R.id.itemspmoi_image);
+        }
+    }
+}
