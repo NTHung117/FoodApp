c628c6ca6b139635a1f5011b71f85e9716b99cc4
NTHung117 <nguyentienhung117@gmail.com>
2023-03-25 22:34:18 +0700

    Đưa dữ liệu vào recycleview màn hình điện thoại

--- app/src/main/java/com/example/foodapp/activity/DienThoaiActivity.java
+++ app/src/main/java/com/example/foodapp/activity/DienThoaiActivity.java
@@ -1,16 +1,90 @@
 package com.example.foodapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.foodapp.R;
+import com.example.foodapp.adapter.DienThoaiAdapter;
+import com.example.foodapp.model.SanPhamMoi;
+import com.example.foodapp.retrofit.ApiBanHang;
+import com.example.foodapp.retrofit.RetrofitClient;
+import com.example.foodapp.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class DienThoaiActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+    ApiBanHang apiBanHang;
+    CompositeDisposable compositeDisposable = new CompositeDisposable(); //Sử dụng compositeDisposable của thư viện rxJava để nhận các phần tử từ Observerable và quản lý tài nguyên
+    int page = 1;
+    int loai;
+    DienThoaiAdapter adapterDt;
+    List<SanPhamMoi> sanPhamMoiList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dien_thoai);
+        apiBanHang = RetrofitClient.getInstance(Utils.BASE_URL).create(ApiBanHang.class);
+        loai = getIntent().getIntExtra("loai",1);
+        AnhXa();
+        ActionToolBar();
+        getData();
+    }
+
+    private void getData() {
+        compositeDisposable.add(apiBanHang.getSanPham(page, loai)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        sanPhamMoiModel -> {
+                            if (sanPhamMoiModel.isSuccess()){
+                                sanPhamMoiList = sanPhamMoiModel.getResult();
+                                adapterDt = new DienThoaiAdapter(getApplicationContext(), sanPhamMoiList);
+                                recyclerView.setAdapter(adapterDt);
+                            }
+                        },
+                        throwable -> {
+                            Toast.makeText(getApplicationContext(),"Kết nối thất bại với server", Toast.LENGTH_LONG).show();
+                        }
+                ));
+    }
+
+    private void ActionToolBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //Tạo sự kiện cho nút back trên Toolbar trong trang loại điện thoại
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    private void AnhXa() {
+        toolbar = findViewById(R.id.toolbar);
+        recyclerView = findViewById(R.id.recyclerView_dt);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        sanPhamMoiList = new ArrayList<>();
+    }
+
+    @Override
+    protected void onDestroy() {
+        compositeDisposable.clear();
+        super.onDestroy();
     }
 }
