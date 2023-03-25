26b3719c6c38683befecbbc7f704b5a02df097d5
NTHung117 <nguyentienhung117@gmail.com>
2023-03-26 02:03:15 +0700

    Tạo loading trong recycleview điện thoại

--- app/src/main/java/com/example/foodapp/adapter/DienThoaiAdapter.java
+++ app/src/main/java/com/example/foodapp/adapter/DienThoaiAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,10 +18,11 @@ import com.example.foodapp.model.SanPhamMoi;
 import java.text.DecimalFormat;
 import java.util.List;
 
-public class DienThoaiAdapter extends RecyclerView.Adapter<DienThoaiAdapter.MyViewHolder> {
+public class DienThoaiAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     Context context;
     List<SanPhamMoi> array;
-
+    private static final int VIEW_TYPE_DATA = 0;
+    private static final int VIEW_TYPE_LOADING = 1;
     public DienThoaiAdapter(Context context, List<SanPhamMoi> array) {
         this.context = context;
         this.array = array;
@@ -28,32 +30,61 @@ public class DienThoaiAdapter extends RecyclerView.Adapter<DienThoaiAdapter.MyVi
 
     @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_dienthoai, parent, false);
-        return new MyViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //Nếu có data t sẽ chạy hàm MyViewHolder để hiển thị dữ liệu lên
+        if (viewType == VIEW_TYPE_DATA){
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_dienthoai, parent, false);
+            return new MyViewHolder(view);
+        }else { //Nếu không có data t sẽ chạy hàm LoadingViewHolder
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_loading, parent, false);
+            return new LoadingViewHolder(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        SanPhamMoi sanPham = array.get(position);
-        holder.tensp.setText(sanPham.getTensanpham());
-        DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
-        holder.giasp.setText("Giá: "+decimalFormat.format(Double.parseDouble(sanPham.getGiasanpham()))+ "đ");
-        holder.mota.setText(sanPham.getMota());
-        Glide.with(context).load(sanPham.getHinhanh()).into(holder.hinhanh);
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof MyViewHolder){
+            MyViewHolder myViewHolder = (MyViewHolder) holder;
+            SanPhamMoi sanPham = array.get(position);
+            myViewHolder.tensp.setText(sanPham.getTensanpham());
+            myViewHolder.idsp.setText(sanPham.getId() + "");
+            DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
+            myViewHolder.giasp.setText("Giá: "+decimalFormat.format(Double.parseDouble(sanPham.getGiasanpham()))+ "đ");
+            myViewHolder.mota.setText(sanPham.getMota());
+            Glide.with(context).load(sanPham.getHinhanh()).into(myViewHolder.hinhanh);
+        }else {
+            LoadingViewHolder loadingViewHolder = (LoadingViewHolder) holder;
+            loadingViewHolder.progressBar.setIndeterminate(true);
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+
+        return array.get(position) == null ? VIEW_TYPE_LOADING:VIEW_TYPE_DATA;
+        //Nếu như cái mảng này t get vị trí của nó bằng null thì sẽ xuất ra VIEW_TYPE_LOADING, còn nếu mảng này t get vị trí nó không bằng null thì sẽ xuất ra là có data VIEW_TYPE_DATA để hiển thị ra dữ liệu
     }
 
     @Override
     public int getItemCount() {
         return array.size();
     }
-
+    //Nếu như dữ liệu null thì chạy cái anh loading này để cho nó xoay xoay
+    public class LoadingViewHolder extends RecyclerView.ViewHolder{
+        ProgressBar progressBar;
+        public LoadingViewHolder(@NonNull View itemView) {
+            super(itemView);
+            progressBar = itemView.findViewById(R.id.progressbar);
+        }
+    }
+    //Nếu như dữ liệu có data thì sẽ add thằng này lên để nó hiện thị dữ liệu
     public class MyViewHolder extends RecyclerView.ViewHolder {
-        TextView tensp, giasp, mota;
+        TextView tensp, giasp, mota, idsp;
         ImageView hinhanh;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             tensp = itemView.findViewById(R.id.itemdt_ten);
+            idsp = itemView.findViewById(R.id.itemdt_idsp);
             giasp = itemView.findViewById(R.id.itemdt_gia);
             mota = itemView.findViewById(R.id.itemdt_mota);
             hinhanh = itemView.findViewById(R.id.itemdt_image);
